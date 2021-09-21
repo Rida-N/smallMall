28dba8aa46b8f00921cc2ae943fb09fbd8e548a5
Rida <nowthursday@gmail.com>
2021-09-21 15:45:01 +0800

    add cart page

--- miniprogram/pages/cart/index.js
+++ miniprogram/pages/cart/index.js
@@ -1,66 +1,40 @@
 // pages/cart/index.js
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
+    address:{},
+    cart:[]
+  },
 
+  onShow(){
+    const address=wx.getStorageSync("address");
+    
+    const cart=wx.getStorageSync("cart")||[];
+    if(address){
+      this.setData({
+        address
+      })
+    }
+
+    // set cart
+    if(cart.length){
+      this.setData({cart})
+    }
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function (options) {},
+
+  handleChooseAdress() {
+    wx.chooseAddress({
+      success: (result) => {
+        wx.setStorageSync("address", result);
+      },
+    });
 
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
-})
+});
